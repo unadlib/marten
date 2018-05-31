bb4a284775070c0dfdd877c463ab0b601e2211bf
unadlib <unadlib@gmail.com>
2018-06-01 02:28:56 +0800

    update

--- examples/automation/features/Meeting/index.spec.js
+++ examples/automation/features/Meeting/index.spec.js
@@ -10,22 +10,25 @@ afterEach(async () => {
 });
 
 describe('Meeting', () => {
-    flow({
-        target: [
-            ['salesforce', ['rc']],
-        ],
-        options: [{
-            topic: 'Test Topic',
-        }]
-    }, (sign, options) => {
-        it(sign(`Create Meeting => defalut`), async () => {
-            process = new Meeting(options);
-            await process.run();
-            await process.input();
-            expect(0).toEqual(0);
-            await process.create();
-            // await expect(0).resolves.toEqual(0);
-            expect(0).toEqual(0);
-        });
-    });
+    flow(
+        {
+            target: [
+                ['salesforce', ['rc']],
+            ],
+            options: [{
+                topic: 'Test Topic',
+            }]
+        },
+        (sign, options) => {
+            it(sign(`Create Meeting => defalut`), async () => {
+                process = new Meeting(options);
+                await process.run();
+                await process.input();
+                expect(0).toEqual(0);
+                await process.create();
+                // await expect(0).resolves.toEqual(0);
+                expect(0).toEqual(0);
+            });
+        }
+    );
 });
